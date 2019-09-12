b983bc6f2a72e51b860e6e27afea2a4da9750fb4
rabiedadi <rabiedadi@gmail.com>
2019-09-12 14:34:29 +0200

    second version of SVM model

--- APIsvm/SVM_project.py
+++ APIsvm/SVM_project.py
@@ -5,38 +5,38 @@ import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler
 
-df = pd.DataFrame(columns=['XP', 'VP', 'XQ', 'VQ', 'XR', 'VR', 'XT', 'VT', 'XSR', 'XTS', 'XPT', 'XQP'])
+df = pd.DataFrame(columns=['XP', 'VP', 'XQ', 'VQ', 'XR', 'VR', 'XT', 'VT', 'XSR', 'XTS', 'XPT', 'XQP', 'XQS'])
 M = ['A', 'L', 'N', 'R', 'V']
+all_predict_result = []
 predict_result = {}
 unresolved_classes = []
+sampling_rate = 360
 
 
-def main_function(file_path):
+def main_function(file_path, rate):
     global df
+    global sampling_rate
+    global unresolved_classes
+    sampling_rate = rate
+
     text_file = open(file_path, "r")
     data = text_file.read().splitlines()
     text_file.close()
 
     data = data[5:]  # data body
     data = np.asarray(data, dtype=np.float)  # read data as float
-    df = pd.DataFrame(columns=['XP', 'VP', 'XQ', 'VQ', 'XR', 'VR', 'XT', 'VT', 'XSR', 'XTS', 'XPT', 'XQP'])
+    df = pd.DataFrame(columns=['XP', 'VP', 'XQ', 'VQ', 'XR', 'VR', 'XT', 'VT', 'XSR', 'XTS', 'XPT', 'XQP', 'XQS'])
+
+    # we don't know how to extract features, so we will use all methods and we take
+    # only the appropriate class percentage with the method , 'A' from the first 'L' from the second and so on
     a_features_detector(data)
     l_features_detector(data)
     n_features_detector(data)
     r_features_detector(data)
     v_features_detector(data)
 
-    classifier = joblib.load('rbf_2000_SVM_model2')
-
-    for i in range(0, df.shape[0]):
-        pred = classifier.predict_proba(df.iloc[[i]])[0] * 100
-        prob_per_class_dict = dict(zip(classifier.classes_, pred))
-        predict_result[M[i]] = prob_per_class_dict[M[i]]
-    for key, value in predict_result.items():
-        predict_result[key] = round(value, 2)
-
-    for m in unresolved_classes:
-        predict_result[m] = 0.0
+    prediction_result_calculating()
+    unresolved_classes = []
     return predict_result
 
 
@@ -52,28 +52,25 @@ def n_features_detector(lines):
 
         P = np.amax(f_part[:-15])
         P_index = np.where(f_part[:-15] == P)[0][0]
+
         Q = np.amin(f_part[-10:])
         Q_index = np.where(f_part[-10:] == Q)[0][0] + len(f_part[:-10])
-        S = np.amin(l_part[:10])
-        S_index = np.where(l_part[:10] == S)[0][0] + len(f_part)
-        T = np.amax(l_part[5:])
-        T_index = np.where(l_part[5:] == T)[0][0] + 5 + len(f_part)
+
+        S = np.amin(l_part[:15])
+        S_index = np.where(l_part[:15] == S)[0][0] + len(f_part)
+
+        T = np.amax(l_part[20:])
+        T_index = np.where(l_part[20:] == T)[0][0] + 20 + len(f_part)
 
         data = {
             'XP': [P_index], 'VP': [P], 'XQ': [Q_index], 'VQ': [Q], 'XR': [R_index], 'VR': [R], 'XT': [T_index],
             'VT': [T],
-            'XSR': [R_index - S_index], 'XTS': [T_index - S_index], 'XPT': [T_index - P_index],
-            'XQP': [Q_index - P_index]
+            'XSR': [(R_index - S_index)], 'XTS': [(T_index - S_index)],
+            'XPT': [(T_index - P_index)], 'XQP': [(Q_index - P_index)],
+            'XQS': [(S_index - Q_index)],
         }
         df1 = pd.DataFrame(data=data)
-        # df1 = df1.loc[:, df1.columns].values
-        # print('df1', df1)
-        # df2 = StandardScaler().fit_transform(df1)
-        # print('df2', df2)
-        # df2 = pd.DataFrame(df2, columns=df1.columns)
-        # print('df2', df2)
         df = df.append(df1, ignore_index=True)
-        # print('df1', df)
     except:
         print('error occur with N features extraction')
         unresolved_classes.append('N')
@@ -89,20 +86,24 @@ def r_features_detector(lines):
         f_part = lines[:R_index]
         l_part = lines[R_index:]
 
-        P = 0
+        P = lines[0]
         P_index = 0
-        Q = np.amax(f_part[-20:])
-        Q_index = np.where(f_part[-20:] == Q)[0][0] + len(f_part[:-20])
+
+        Q = np.amax(f_part)
+        Q_index = np.where(f_part == Q)[0][0]
+
         S = np.amax(l_part[:20])
         S_index = np.where(l_part[:20] == S)[0][0] + len(f_part)
-        T = np.amin(l_part)
+
+        T = np.amin(l_part[S_index - len(f_part):])
         T_index = np.where(l_part == T)[0][0] + len(f_part)
 
         data = {
             'XP': [P_index], 'VP': [P], 'XQ': [Q_index], 'VQ': [Q], 'XR': [R_index], 'VR': [R], 'XT': [T_index],
             'VT': [T],
-            'XSR': [R_index - S_index], 'XTS': [T_index - S_index], 'XPT': [T_index - P_index],
-            'XQP': [Q_index - P_index]
+            'XSR': [(R_index - S_index)], 'XTS': [(T_index - S_index)],
+            'XPT': [(T_index - P_index)], 'XQP': [(Q_index - P_index)],
+            'XQS': [(S_index - Q_index)],
         }
         df1 = pd.DataFrame(data=data)
         # df2 = StandardScaler().fit_transform(df1)
@@ -117,25 +118,27 @@ def a_features_detector(lines):
     try:
         global R_index
         global df
-        R = np.amax(lines)
-        R_index = np.where(lines == R)[0][0]
-        f_part = lines[:R_index]
-        l_part = lines[R_index:]
+        Q = np.amax(lines)
+        Q_index = np.where(lines == Q)[0][0]
 
-        P = np.amax(f_part[:-10])
-        P_index = np.where(f_part[:-10] == P)[0][0]
-        Q = np.amin(f_part[-10:])
-        Q_index = np.where(f_part[-10:] == Q)[0][0] + len(f_part[:-10])
-        S = np.amin(l_part[:10])
-        S_index = np.where(l_part[:10] == S)[0][0] + len(f_part)
-        T = np.amax(l_part[5:])
-        T_index = np.where(l_part[5:] == T)[0][0] + 5 + len(f_part)
+        R = np.amin(lines[Q_index:Q_index + 20])
+        R_index = np.where(lines[Q_index:Q_index + 20] == R)[0][0] + Q_index
+
+        P = np.amin(lines[:Q_index])
+        P_index = np.where(lines[:Q_index] == P)[0][0]
+
+        S = np.amax(lines[R_index: R_index + 20])
+        S_index = np.where(lines[R_index: R_index + 20] == S)[0][0] + R_index
+
+        T = np.amin(lines[S_index:])
+        T_index = np.where(lines[S_index:] == T)[0][0] + S_index
 
         data = {
             'XP': [P_index], 'VP': [P], 'XQ': [Q_index], 'VQ': [Q], 'XR': [R_index], 'VR': [R], 'XT': [T_index],
             'VT': [T],
-            'XSR': [R_index - S_index], 'XTS': [T_index - S_index], 'XPT': [T_index - P_index],
-            'XQP': [Q_index - P_index]
+            'XSR': [(R_index - S_index)], 'XTS': [(T_index - S_index)],
+            'XPT': [(T_index - P_index)], 'XQP': [(Q_index - P_index)],
+            'XQS': [(S_index - Q_index)],
         }
         df1 = pd.DataFrame(data=data)
         # df2 = StandardScaler().fit_transform(df1)
@@ -158,18 +161,22 @@ def l_features_detector(lines):
 
         Q = np.amax(f_part[-20:])
         Q_index = np.where(f_part[-20:] == Q)[0][0] + len(f_part[:-20])
+
         P = np.amin(f_part[:Q_index])
         P_index = np.where(f_part == P)[0][0]
+
         T = np.amax(l_part)
         T_index = np.where(l_part == T)[0][0] + len(f_part)
+
         S = derivative(l_part[10:T_index])
         S_index = np.where(l_part[10:T_index] == S)[0][0] + len(f_part)
 
         data = {
             'XP': [P_index], 'VP': [P], 'XQ': [Q_index], 'VQ': [Q], 'XR': [R_index], 'VR': [R], 'XT': [T_index],
             'VT': [T],
-            'XSR': [R_index - S_index], 'XTS': [T_index - S_index], 'XPT': [T_index - P_index],
-            'XQP': [Q_index - P_index]
+            'XSR': [(R_index - S_index)], 'XTS': [(T_index - S_index)],
+            'XPT': [(T_index - P_index)], 'XQP': [(Q_index - P_index)],
+            'XQS': [(S_index - Q_index)],
         }
         df1 = pd.DataFrame(data=data)
         # df2 = StandardScaler().fit_transform(df1)
@@ -184,32 +191,35 @@ def v_features_detector(lines):
     try:
         global R_index
         global df
-        R = np.amax(lines)
+        P = 0
+        P_index = 0
+
+        R = np.amin(lines)
         R_index = np.where(lines == R)[0][0]
 
         f_part = lines[:R_index]
         l_part = lines[R_index:]
 
-        P = np.amax(f_part[:-20])
-        P_index = np.where(f_part == P)[0][0]
-        Q = np.amin(f_part)
+        Q = np.amax(f_part)
         Q_index = np.where(f_part == Q)[0][0]
-        S = np.amin(l_part[:30])
-        S_index = np.where(l_part[:30] == S)[0][0] + len(f_part)
+
+        S = np.amax(l_part)
+        S_index = np.where(l_part == S)[0][0] + len(f_part)
+
         T = np.amax(l_part[30:])
         T_index = np.where(l_part[30:] == T)[0][0] + 30 + len(f_part)
 
         data = {
             'XP': [P_index], 'VP': [P], 'XQ': [Q_index], 'VQ': [Q], 'XR': [R_index], 'VR': [R], 'XT': [T_index],
             'VT': [T],
-            'XSR': [R_index - S_index], 'XTS': [T_index - S_index], 'XPT': [T_index - P_index],
-            'XQP': [Q_index - P_index]
+            'XSR': [(R_index - S_index)], 'XTS': [(T_index - S_index)],
+            'XPT': [(T_index - P_index)], 'XQP': [(Q_index - P_index)],
+            'XQS': [(S_index - Q_index)],
         }
         df1 = pd.DataFrame(data=data)
         # df2 = StandardScaler().fit_transform(df1)
         # df2 = pd.DataFrame(df2, columns=df1.columns)
         df = df.append(df1, ignore_index=True)
-        unresolved_classes.append('H')
     except:
         print('error occur with V features extraction')
         unresolved_classes.append('V')
@@ -221,3 +231,23 @@ def derivative(data):
         if y < 0:
             break
     return data[i]
+
+
+def prediction_result_calculating():
+    global predict_result
+    classifier = joblib.load('rbf_2500_3')  # the classifier for predicting
+    shift = 0
+
+    for i in range(0, len(M)):
+        # this is for unresolved methods so we take 0 instead of taking result of other method
+        resolved = False
+        for u in unresolved_classes:
+            if i == M.index(u):
+                shift = shift + 1
+                resolved = True  # to break the parent loop
+                predict_result[M[i]] = 0.0
+        if resolved:
+            continue
+        pred = classifier.predict_proba(df.iloc[[i - shift]])[0] * 100
+        prob_per_class_dict = dict(zip(classifier.classes_, pred))  # append the result with the class
+        predict_result[M[i]] = prob_per_class_dict[M[i]]
--- APIsvm/SVM_server.py
+++ APIsvm/SVM_server.py
@@ -1,29 +1,51 @@
 import os
-from flask import Flask, request, url_for, send_from_directory
+import shutil
+from glob import glob
+from flask import Flask, request
 import werkzeug
 import SVM_project
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
-    # this has changed from the original example because the original did not work for me
-    return filename[-3:].lower() in ALLOWED_EXTENSIONS
+    return filename[-3:].lower() in {'zip'}
 
 
-@app.route('/', methods=['GET', 'POST'])
+def get_final_result(results):
+    final_result = {'A': 0, 'L': 0, 'N': 0, 'R': 0, 'V': 0}
+    for result in results:
+        for key in final_result:
+            final_result[key] = result[key] + final_result[key]
+    for key in final_result:
+        final_result[key] /= results.__len__()
+    final_result['P'] = 0.0
+    for key, value in final_result.items():
+        final_result[key] = value * 0.98
+        final_result[key] = round(final_result[key], 2)
+    return final_result
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['data']
         if file and allowed_file(file.filename):
-            filename = werkzeug.secure_filename(file.filename)
-            file.save('uploads/' + filename)
-            x = SVM_project.main_function(file_path='uploads/' + filename)
-            return x
+            basePath = os.path.dirname(__file__)
+            file_path = os.path.join(basePath, 'uploads', werkzeug.secure_filename('data.zip'))
+            file.save(file_path)
+            shutil.unpack_archive('uploads/data.zip', extract_dir='extracted_files')
+            paths = glob('extracted_files/*.txt')
+            result = []
+            for path in paths:
+                result.append(SVM_project.main_function(path, 360))
+            #  remove files to avoid confusing with the next prediction
+            shutil.rmtree('extracted_files')
+            os.remove('uploads/data.zip')
+            return get_final_result(result)
     return '''
         <title>Upload new File</title>
         <h1>Upload new File</h1>
@@ -34,12 +56,5 @@ def upload_file():
     '''
 
 
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     print(filename)
-#     SVM_project.main_function(file_path='uploads/' + filename)
-#     return "done"
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
